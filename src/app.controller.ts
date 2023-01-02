0b995e9e4b7a103a90d32a015ce8c2c5c7266e5e
L2HYUNN <donghyuni98@gmail.com>
2023-01-02 23:44:37 +0900

    feat: add nest modules

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,12 @@
-import { Body, Controller, Get, Param, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('cats')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello/:id/:name')
-  getHello(
-    @Req() req: Request,
-    @Body() body,
-    @Param() param: { id: string; name: string },
-  ): string {
-    console.log(req);
+  @Get()
+  getHello(): string {
     return this.appService.getHello();
   }
 }
