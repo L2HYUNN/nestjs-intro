4dac2bf5d16796e5e26def9b501b1cfa102b0b7e
L2HYUNN <donghyuni98@gmail.com>
2023-01-17 02:22:29 +0900

    feat: complete repository pattern

--- src/cats/cats.repository.ts
+++ src/cats/cats.repository.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Cat } from './cats.schema';
@@ -9,12 +9,8 @@ export class CatsRepository {
   constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
 
   async existsByEmail(email: string): Promise<boolean> {
-    try {
-      const result = await this.catModel.exists({ email });
-      return result ? true : false;
-    } catch (e) {
-      throw new HttpException('DB Error', 400);
-    }
+    const result = await this.catModel.exists({ email });
+    return result ? true : false;
   }
 
   async create(cat: CatRequestDto): Promise<Cat> {
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,8 +1,5 @@
 import * as bcrypt from 'bcrypt';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Cat } from './cats.schema';
 import { CatRequestDto } from './dto/cats.request.dto';
 import { CatsRepository } from './cats.repository';
 
